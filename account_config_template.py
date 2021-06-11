0989c7c375109d03c2b525dd68914802ac1e105f
KIST-KIMS <41173823+kim1102@users.noreply.github.com>
2021-06-11 19:52:19 +0900

    account configurations

---
+++ account_config_template.py
@@ -0,0 +1,11 @@
+"""
+devkim1102@gmail.com
+!!!access key 노출 금지!!!
+"""
+
+def get_account():
+    # ID settings
+    access_key = "" # your access key
+    secret_key = "" # your secret key
+
+    return access_key, secret_key
