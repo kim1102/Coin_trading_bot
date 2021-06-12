f226412e360fb35f7715e8d0ed0d0eb075a41290
KIST-KIMS <41173823+kim1102@users.noreply.github.com>
2021-06-12 12:25:57 +0900

    new method: get top attention coin

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import csv
 import requests
 import json
+import pyupbit as pybit
+from time import sleep
 
 def get_safe_coin_list():
     safe_coins = []
@@ -18,5 +20,21 @@ def get_safe_coin_list():
 
     return result
 
+def get_top_attention_coin(total_coin_list):
+    # return top10 raising coins
+    attention_score = []
+    for ticker in total_coin_list:
+        day_candle = pybit.get_ohlcv(ticker, interval="day")['close']
+        attention_score.append(day_candle[-1]/day_candle[-2])
+        sleep(0.3)
+
+    res = sorted(range(len(attention_score)), key=lambda sub: attention_score[sub])[-10:]
+    top_attention_list = [total_coin_list[idx] for idx in res]
+
+    return top_attention_list
+
+
 if __name__ == '__main__':
-    get_safe_coin_list()
+    safe_list = get_safe_coin_list()
+    top_volume = get_top_attention_coin(safe_list)
+    print(top_volume)
