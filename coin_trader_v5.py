5f253024db44fbe5c8399c2ee08c6ac7a4ca0f94
KIST-KIMS <41173823+kim1102@users.noreply.github.com>
2021-06-13 16:37:56 +0900

    bug-fix & update

--- coin_trader_v5.py
+++ coin_trader_v5.py
@@ -15,16 +15,23 @@ from datetime import datetime
 from easydict import EasyDict as edict
 import pandas
 import time
-from utils import get_safe_coin_list, get_top_attention_coin, get_volume_percentage
+from utils import get_safe_coin_list, get_top_attention_coin, get_10min_volume_percentage
 
 def get_config():
     conf = edict()
     # === parameter: win
-    conf.unit = 30000
+    conf.unit_per = 0.5
     conf.margin_per = 0.02
     # === parameter: loss
-    conf.loss_cut = 0.007
-    conf.base_candle = "minute1"
+    conf.loss_cut = 0.015
+    conf.base_candle = "minute3"
+    # === parameter: timed out
+    conf.timed_out = 30  # 30minute for waiting
+
+    # === parameter: entrance gap
+    # previous price < moving average(MA) - MA * param &&
+    # current price > MA + MA * param -> then buy
+    conf.entrance_gap = 0.002
 
     # === parameter: else
     conf.trade_interval = 5# default 1min
@@ -41,15 +48,18 @@ class trading_bot():
         self.initial_krw = 0
 
         conf = get_config()
-        self.unit = conf.unit
+        self.unit = 0
+        self.unit_per = conf.unit_per
         self.interval = conf.trade_interval
         self.margin_per = conf.margin_per
+        self.entrance_gap = conf.entrance_gap
         self.loss_cut = conf.loss_cut
         self.base_candle = conf.base_candle
+        self.timed_out = conf.timed_out
 
         # for log write
         self.log_win_count = 0
-        self.log_loss_cut_count = 0
+        self.log_time_out_count = 0
         self.log_loss_count = 0
 
         current_time = time.strftime('%Y-%m-%d', time.localtime(time.time()))
@@ -73,14 +83,18 @@ class trading_bot():
         # cumulative win strategy
         try:
             while True:
+                # unit update
+                self.unit = pybit.get_tick_size(self.session.get_balance('KRW')*self.unit_per)
+                sleep(0.5)
+
                 # wait until entrance
-                self.target_coin = self.ready_to_buy()
+                self.target_coin, current_price = self.ready_to_buy()
 
                 # trade start
-                self.trade_target_coin()
+                self.trade_target_coin(current_price)
 
                 log = f"[Betting history] win: {self.log_win_count}/ " \
-                      f"loss: {self.log_loss_count}/ loss cut: {self.log_loss_cut_count}"
+                      f"loss: {self.log_loss_count}/ timed out: {self.log_time_out_count}"
                 self.write_log(log)
 
                 log = f'[Trade complete]=== Earning rate: {self.cal_ER():0,.2f}%'
@@ -108,62 +122,82 @@ class trading_bot():
                 coin_list = random.sample(self.coin_list, 5)
 
                 for ticker in coin_list:
-                    moving_avg, current_open, current_close = self.cal_ma(ticker, interval=self.base_candle)
-                    condition1 = current_open < moving_avg
-                    condition2 = moving_avg < current_close
-                    condition3 = get_volume_percentage(ticker) > 1.5
+                    moving_avg, previous_price, current_close = self.cal_ma(ticker, interval=self.base_candle)
+                    condition1 = previous_price < moving_avg - moving_avg*self.entrance_gap
+                    condition2 = moving_avg + moving_avg*self.entrance_gap*0.5 < current_close
+                    #percentage = get_10min_volume_percentage(ticker)
+                    #condition3 = percentage > 1.5
 
-                    if condition1 and condition2 and condition3:
+                    if condition1 and condition2:
                         log = f'!=== Start trading / Target coin:{ticker}'
                         self.write_log(log)
-                        log = f'current_price: {current_close:.2f}/ previous_price: {current_open:.2f}/ ' \
+                        log = f'current_price: {current_close:.2f}/ previous_price: {previous_price:.2f}/ ' \
                               f'moving average: {moving_avg:.2f}'
                         self.write_log(log)
 
-                        return ticker
+                        #print(percentage)
+
+                        return ticker, current_close
 
                 sleep(1)
         except KeyboardInterrupt:
             self.terminate_session()
 
 
-    def cal_ma(self, ticker, interval="minute3", period=15):
+    def cal_ma(self, ticker, interval="minute3", period=50):
         candle_data = pybit.get_ohlcv(ticker, interval)
         closedata = candle_data['close']
         moving_avg = sum(closedata.tolist()[-period:])/period
         current_close = closedata[-1]
+
+        # get previous price
+        previous_high = candle_data['high'][-2]
         current_open = candle_data['open'][-1]
+        previous_price = (previous_high + current_open)/2
 
-        return moving_avg, current_open, current_close
+        return moving_avg, previous_price, current_close
 
 
-    def trade_target_coin(self):
-        self.session.buy_market_order(ticker=self.target_coin, price=self.unit)
+    def trade_target_coin(self, start_price):
+        print(self.session.buy_market_order(ticker=self.target_coin, price=self.unit))
         while True:
             sleep(0.5)
             if len(self.session.get_order(self.target_coin)) < 1:  # buy complete
                 break
 
         volume = self.session.get_balance(self.target_coin)
-        current_price = self.session.get_avg_buy_price(self.target_coin)
-        loss_price = current_price - current_price*self.loss_cut
-        sell_order = self.reserve_order(current_price=current_price, volume=volume)
+        sleep(0.3)
+        buy_price = self.session.get_avg_buy_price(self.target_coin)
+        loss_price = buy_price - buy_price*self.loss_cut
+        sleep(0.3)
+        sell_order = self.reserve_order(current_price=buy_price, volume=volume)
         sell_id = sell_order['uuid']
+        sleep(0.3)
 
+        t1 = time.time()
         try:
             while True:
                 current_price = pybit.get_current_price(self.target_coin)
+                sleep(0.5)
                 # lost
                 if current_price < loss_price:
-                    self.session.sell_market_order(ticker=self.target_coin, volume=volume)
-                    self.session.cancel_order(sell_id)
+                    print(self.session.cancel_order(sell_id))
+                    print(self.session.sell_market_order(ticker=self.target_coin, volume=volume))
+                    sleep(0.5)
                     self.log_loss_count += 1
-                    break
+                    return
                 # win
                 elif len(self.session.get_order(self.target_coin)) < 1:
                     self.log_win_count += 1
-                    break
-                sleep(0.5)
+                    return
+                # timed out
+                elif time.time() - t1 > self.timed_out*60:
+                    print(self.session.cancel_order(sell_id))
+                    print(self.session.sell_market_order(ticker=self.target_coin, volume=volume))
+                    sleep(0.5)
+                    self.log_time_out_count += 1
+                    return
+
 
         except KeyboardInterrupt:
             self.terminate_session()
@@ -183,9 +217,7 @@ class trading_bot():
 
     def reserve_order(self, current_price, volume):
         win_price = pybit.get_tick_size(current_price + current_price * self.margin_per)
-        sell_order = self.session.sell_limit_order(ticker=self.target_coin,
-                                                   price=win_price,
-                                                   volume=volume)
+        sell_order = self.session.sell_limit_order(ticker=self.target_coin, price=win_price, volume=volume)
         return sell_order
 
 
